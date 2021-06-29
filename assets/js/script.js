dbee0eae61d864815ccb39f95cf03891b66eb329
Matilda Brantley <matildahbrantley@gmail.com>
2021-06-29 10:01:07 -0400

    project skeleton, basic API calls

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+async function getWikiData(page) {
+    var url = "https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&generator=search&gsrnamespace=0&gsrlimit=5&gsrsearch=" + page;
+    //return fetch Promise of Wiki page
+    return fetch(url)
+    .then(function (response) {
+        //console.log(response);
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data.query.pages);
+
+        return data.query.pages;
+    });
+};
+
+getWikiData("California");
+
+async function getCityBreweryData(city) {
+    var url = "https://api.openbrewerydb.org/breweries?by_city=" + city;
+    //return fetch Promise of city's brewery data
+    return fetch(url)
+    .then(function (response) {
+        //console.log(response);
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+
+        return data;
+    });
+};
+
+getCityBreweryData("Atlanta");
