7a6794b88fadef962b00837a5c936a76af9f95da
Andrew Koster <andrewkoster95@gmail.com>
2021-06-30 14:02:04 -0400

    added functions to help display search results on search-results.html

--- assets/js/display-search.js
+++ assets/js/display-search.js
@@ -46,4 +46,78 @@ function printResults(resultObj) {
       resultBody.append(title, bodyContent, linkButton);
     
       resultContent.append(resultCard);
+  
+  }
+
+  function searchApi(query, format) {
+    var locQueryUrl = "https://api.openbrewerydb.org/breweries/search?query=dog";
+  
+    if (format) {
+      locQueryUrl = "https://api.openbrewerydb.org/" + format + "?query=dog";
     }
+  
+    locQueryUrl = locQueryUrl + '&q=' + query;
+  
+    fetch(locQueryUrl)
+      .then(function (response) {
+        if (!response.ok) {
+          throw response.json();
+        }
+  
+        return response.json();
+      })
+      .then(function (locRes) {
+        resultText.textContent = locRes.search.query;
+  
+        console.log(locRes);
+  
+        if (!locRes.results.length) {
+          console.log('No results found!');
+          resultContentEl.innerHTML = '<h3>No results found, search again!</h3>';
+        } else {
+          resultContentEl.textContent = '';
+          for (var i = 0; i < locRes.results.length; i++) {
+            printResults(locRes.results[i]);
+          }
+        }
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }
+  
+
+  function handleSearchFormSubmit(event) {
+    event.preventDefault();
+  
+    var searchInputVal = document.querySelector('#search-input').value;
+    var formatInputVal = document.querySelector('#format-input').value;
+  
+    if (!searchInputVal) {
+      console.error('You need a search input value!');
+      return;
+    }
+  
+    searchApi(searchInputVal, formatInputVal);
+  }
+  
+
+  async function getBreweryData(region, regionType) {
+    var url = "https://api.openbrewerydb.org/breweries?by_"+ regionType + "=" + region;
+    //return fetch Promise of region's brewery data
+    return fetch(url)
+    .then(function (response) {
+        //console.log(response);
+        return response.json();
+    })
+    .then(function (data) {
+        console.log("Region results");
+        console.log(data);
+
+        return data;
+    });
+};
+
+searchForm.addEventListener('submit', handleSearchFormSubmit);
+
+getParams();
