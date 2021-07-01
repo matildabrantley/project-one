3e63d7b5dab94291030d61bb0c570b645b4c223f
eagle <eagle.lam.le@Gmail.com>
2021-07-01 12:03:09 -0400

    submit

--- assets/js/Phone.js
+++ assets/js/Phone.js
@@ -1,42 +1,3 @@
-var Button1 = $('#Hill-Farmstead-Brewery-Phone');
-var Button2 = $('#Side-Project-Brewing-Phone');
-var Button3 = $('#Trillium-Brewing-Company-Phone');
-var Button4 = $('#Tree-House-Brewing-Company-Phone');
-var Button5 = $('#Cigar-City-Brewing-Phone');
-var Button6 = $('#AleSmith-Brewing-Company-Phone');
-var Button7 = $('#Founders-Brewing-Company-Phone');
-var Button8 = $('#Cloudwater-Brew-Co-Phone');
-var Button9 = $('#Sante-Adairius-Rustic-Ales-Phone');
-var Button10 = $('#Firestone-Walker-Brewing-Phone');
-
-Button1.on('click', function () {
-    alert('Phone Number: (802) 533-7450');
-  });
-  Button2.on('click', function () {
-    alert('Phone Number: 314-224-5211');
-  });
-  Button3.on('click', function () {
-    alert('Phone Number: (857) 449-0083');
-  });
-  Button4.on('click', function () {
-    alert('Phone Number: (413) 523-2367');
-  });
-  Button5.on('click', function () {
-    alert('Phone Number: (813) 348-6363');
-  });
-  Button6.on('click', function () {
-    alert('Phone Number: (858) 549-9888');
-  });
-  Button7.on('click', function () {
-    alert('Phone Number: (616) 776-1195');
-  });
-  Button8.on('click', function () {
-    alert('Phone Number: +44 161 661 5943');
-  });
-  Button9.on('click', function () {
-    alert('Phone Number: (831) 462-1227');
-  });
-
   function website1() {
     window.open("https://hillfarmstead.com/", "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=100,left=500,width=800,height=800");
   }
@@ -75,7 +36,7 @@ Button1.on('click', function () {
     window.open("https://goo.gl/maps/yBhBp8ZCBn7pji389", "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=100,left=500,width=800,height=800");
   }
   function map4() {
-    window.open("https://goo.gl/maps/PUXsx66hNGD14yL66", "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=100,left=500,width=800,height=800");
+    window.open("https://goo.gl/maps/Sro6nAYXG5UwuVNs9", "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=100,left=500,width=800,height=800");
   }
   function map5() {
     window.open("https://goo.gl/maps/5qDHMRd3cpqUuuQX9", "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=100,left=500,width=800,height=800");
@@ -93,3 +54,39 @@ Button1.on('click', function () {
     window.open("https://goo.gl/maps/LHrq47Notu4ogAa37", "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=100,left=500,width=800,height=800");
   }
 
+  function myFunction() {
+    var popup = document.getElementById("myPopup1");
+    popup.classList.toggle("show");
+  }
+  function myFunction1() {
+    var popup = document.getElementById("myPopup2");
+    popup.classList.toggle("show");
+  }
+  function myFunction3() {
+    var popup = document.getElementById("myPopup3");
+    popup.classList.toggle("show");
+  }
+  function myFunction4() {
+    var popup = document.getElementById("myPopup4");
+    popup.classList.toggle("show");
+  }
+  function myFunction5() {
+    var popup = document.getElementById("myPopup5");
+    popup.classList.toggle("show");
+  }
+  function myFunction6() {
+    var popup = document.getElementById("myPopup6");
+    popup.classList.toggle("show");
+  }
+  function myFunction7() {
+    var popup = document.getElementById("myPopup7");
+    popup.classList.toggle("show");
+  }
+  function myFunction8() {
+    var popup = document.getElementById("myPopup8");
+    popup.classList.toggle("show");
+  }
+  function myFunction9() {
+    var popup = document.getElementById("myPopup9");
+    popup.classList.toggle("show");
+  }
